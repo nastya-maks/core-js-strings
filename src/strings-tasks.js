120b293581ef3521310f14c900911ce1237742f8
nastya-maks <nastyamaksfl@gmail.com>
2024-11-05 22:56:16 +0300

    feat: complete part of tasks

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -180,8 +180,14 @@ function removeLastOccurrences(str, value) {
  *   sumOfCodes('') => 0
  *   sumOfCodes() => 0
  */
-function sumOfCodes(/* str */) {
-  throw new Error('Not implemented');
+function sumOfCodes(str) {
+  if (str === null || str === undefined) {
+    return 0;
+  }
+  const sum = str.split('').reduce((acc, el) => {
+    return acc + el.charCodeAt();
+  }, 0);
+  return sum;
 }
 
 /**
@@ -195,8 +201,8 @@ function sumOfCodes(/* str */) {
  *   startsWith('Hello World', 'World') => false
  *   startsWith('Hello World', 'Hello') => true
  */
-function startsWith(/* str, substr */) {
-  throw new Error('Not implemented');
+function startsWith(str, substr) {
+  return str.startsWith(substr);
 }
 
 /**
@@ -210,8 +216,8 @@ function startsWith(/* str, substr */) {
  *   endsWith('Hello World', 'World') => true
  *   endsWith('Hello World', 'Hello') => false
  */
-function endsWith(/* str, substr */) {
-  throw new Error('Not implemented');
+function endsWith(str, substr) {
+  return str.endsWith(substr);
 }
 
 /**
